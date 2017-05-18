b19e36316bfc91befa20e77b10cab2ed97678505
Daniel Czerwonk <daniel@dan-nrw.de>
2017-05-18 10:55:27 +0200

    added rtt metric for traceroute

--- traceroute/traceroute_metric.go
+++ traceroute/traceroute_metric.go
@@ -11,32 +11,38 @@ type TracerouteMetric struct {
 	ProbeId  int
 	HopCount int
 	Success  int
+	Rtt      float64
 	Asn      int
 }
 
 func FromResult(r *measurement.Result) *TracerouteMetric {
-	success := getTracerouteSuccess(r)
-	return &TracerouteMetric{ProbeId: r.PrbId(), HopCount: len(r.TracerouteResults()), Success: success}
+	m := &TracerouteMetric{ProbeId: r.PrbId(), HopCount: len(r.TracerouteResults())}
+	processLastHop(r, m)
+
+	return m
 }
 
-func getTracerouteSuccess(r *measurement.Result) int {
-	success := 0
-
-	if len(r.TracerouteResults()) > 0 {
-		last := r.TracerouteResults()[len(r.TracerouteResults())-1]
-		for _, rep := range last.Replies() {
-			if rep.From() == r.DstAddr() {
-				success = 1
-			}
-		}
+func processLastHop(r *measurement.Result, m *TracerouteMetric) {
+	if len(r.TracerouteResults()) == 0 {
+		return
 	}
 
-	return success
+	last := r.TracerouteResults()[len(r.TracerouteResults())-1]
+	for _, rep := range last.Replies() {
+		if rep.From() == r.DstAddr() {
+			m.Success = 1
+			m.Rtt = rep.Rtt()
+		}
+	}
 }
 
 func (t *TracerouteMetric) Write(w io.Writer, pk string) {
 	t.writeMetric(pk, "hops", t.HopCount, w)
 	t.writeMetric(pk, "success", t.Success, w)
+
+	if t.Rtt > 0 {
+		t.writeMetric(pk, "rtt", t.Rtt, w)
+	}
 }
 
 func (t *TracerouteMetric) writeMetric(pk string, name string, value interface{}, w io.Writer) {
