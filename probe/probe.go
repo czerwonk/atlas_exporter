2978f9c6a8951165db6ec1cc764a5ef7bd4a0a72
Daniel Czerwonk <daniel@dan-nrw.de>
2022-11-22 17:13:15 +0100

    please linter

--- probe/probe.go
+++ probe/probe.go
@@ -5,7 +5,7 @@ import (
 	"strconv"
 )
 
-const IPv6 int = 6
+const ipv6 int = 6
 
 // Probe holds information about a single Atlas probe
 type Probe struct {
@@ -32,7 +32,7 @@ func FromJSON(body []byte) (*Probe, error) {
 
 // ASNForIPVersion return the ASN for the given IP Version
 func (p *Probe) ASNForIPVersion(v int) int {
-	if v == IPv6 {
+	if v == ipv6 {
 		return p.Asn6
 	}
 
